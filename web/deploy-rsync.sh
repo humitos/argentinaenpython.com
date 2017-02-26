e44865d97a89e781ce463ce9bfa2f64819829383
Manuel Kaufmann <humitos@gmail.com>
2017-02-25 19:00:59 -0500

    Comando para deployar el tutorial con GitBook

--- web/deploy-rsync.sh
+++ web/deploy-rsync.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+# create the GitBook Django Girls Tutorial Translation
+cd ../djangogirls/tutorial/our/es/
+./node_modules/.bin/gitbook install
+./node_modules/.bin/gitbook build
+cd -
+
+# create symbolic link to the GitBook Django Girls Tutorial Translation
+rm output/django-girls/gitbook
+ln -sf ../../../djangogirls/tutorial/our/es/_book output/django-girls/gitbook
+
 rsync --progress --human-readable --recursive --links --verbose --update --compress --delete-after --exclude-from=rsync.exclude --copy-links --bwlimit=250 --stats --partial --partial-dir=/tmp output/* humitos@elblogdehumitos.com:apps/argentinaenpython.com.ar/
 
 rsync --progress --human-readable --recursive --links --verbose --update --compress --delete-after --exclude-from=rsync.exclude --copy-links --bwlimit=50 --stats --partial --partial-dir=/tmp output/* alarm@raspberrypi.redlibre:~/apps/argentinaenpython.com.ar/

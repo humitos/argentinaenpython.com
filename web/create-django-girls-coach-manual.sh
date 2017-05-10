1ede866b7082649d7f22555a2662c5c3c9d101d3
Manuel Kaufmann <humitos@gmail.com>
2017-05-10 02:47:40 -0300

    Use proper PATH variable to build gitbook

--- web/create-django-girls-coach-manual.sh
+++ web/create-django-girls-coach-manual.sh
@@ -2,19 +2,21 @@
 
 set -e
 
+PATH=./node_modules/.bin:$PATH
+
 # HTML
 
 # create the GitBook Django Girls Coach Manual
 cd ../djangogirls/coach-manual/traducidos
-# npm install gitbook-cli
-# ./node_modules/.bin/gitbook install
-./node_modules/.bin/gitbook build
+npm install gitbook-cli
+gitbook install
+gitbook build
 
 
 # PDF
 # this needs calibre since it uses `ebook-convert`
 # https://trello.com/c/JWgZfauR/24-create-pdf-con-gitbook
-./node_modules/.bin/gitbook pdf
+gitbook pdf
 mkdir -p /home/humitos/Source/argentinaenpython.com.ar/web/@djangogirls/pdf
 mv book.pdf _book/django-girls-coach.es.pdf
 cp _book/django-girls-coach.es.pdf /home/humitos/Source/argentinaenpython.com.ar/web/@djangogirls/pdf/

88f0425179fce99feac91ab20b3f050b5e3d82fd
Manuel Kaufmann <humitos@gmail.com>
2019-12-29 12:48:34 +0100

    No copiar a redlibre

--- web/deploy-rsync.sh
+++ web/deploy-rsync.sh
@@ -4,4 +4,4 @@ set -e
 
 rsync --progress --human-readable --recursive --links --verbose --update --compress --delete-after --exclude-from=rsync.exclude --copy-links --bwlimit=250 --stats --partial --partial-dir=/tmp output/* humitos@elblogdehumitos.com:apps/argentinaenpython.com.ar/
 
-rsync --progress --human-readable --recursive --links --verbose --update --compress --delete-after --exclude-from=rsync.exclude --copy-links --bwlimit=50 --stats --partial --partial-dir=/tmp output/* alarm@raspberrypi.redlibre:~/apps/argentinaenpython.com.ar/
+# rsync --progress --human-readable --recursive --links --verbose --update --compress --delete-after --exclude-from=rsync.exclude --copy-links --bwlimit=50 --stats --partial --partial-dir=/tmp output/* alarm@raspberrypi.redlibre:~/apps/argentinaenpython.com.ar/
